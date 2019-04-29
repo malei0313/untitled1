e5ecbf2686608d65e15b020f1580c8a9962bca8d
stone <642792741@qq.com>
2019-04-29 10:43:44 +0800

    hello world - test

---
+++ stu/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+
+
+class IndexView(View):
+    def get(self,request):
+        return HttpResponse('hello world')
